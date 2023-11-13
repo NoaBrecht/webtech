7d7481f3f5a9daf8befedc10436b232206f8233c
NoaBrecht <99073951+NoaBrecht@users.noreply.github.com>
2023-11-13 14:45:58 +0100

    Add JavaScript files for exercise 2 and 3

---
+++ labo_13/deel_02/oefening_03/js/script.js
@@ -0,0 +1,11 @@
+/* Hier komt jouw JavaScript-code */
+let weightMounrir = 80;
+let heightMounrir = 1.7;
+
+let weightJhon = 50;
+let heightJhon = 1.85;
+let bmiJhon = weightJhon / (heightJhon * heightJhon);
+let bmiMounrir = weightMounrir / (heightMounrir * heightMounrir);
+
+console.log(`De BMI van Mounir is ${Math.round(bmiMounrir)}`);
+console.log(`De BMI van Jhon is ${Math.round(bmiJhon)}`);
